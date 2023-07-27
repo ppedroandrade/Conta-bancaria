78f7f930a299c50e95191866ebb8a35bfd931876
ppedroandrade <pedrosalvaro09@gmail.com>
2023-07-26 23:21:42 -0300

    finalizado

--- src/main/java/org/example/Conta_Bancaria.java
+++ src/main/java/org/example/Conta_Bancaria.java
@@ -1,6 +1,4 @@
 package org.example;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Conta_Bancaria {
@@ -13,24 +11,25 @@ public class Conta_Bancaria {
         this.nome_completo = nome_completo;
         this.saldo = saldo;
     }
-
-    public String getNome_completo() {
-        return nome_completo;
+    public void sacar(){
+        System.out.println("Digite o valor que deseja sacar : ");
+        Scanner input = new Scanner (System.in);
+        double saque = input.nextDouble();
+        if(saque <= saldo){
+            saldo -= saque;
+            System.out.println("Seu saldo é :"+ saldo);
+        }else{
+            System.out.println("Voce nao tem saldo pra sacar");
+        }
     }
+    public void deposito(){
+        System.out.println("Digite o valor que deseja depositar : ");
+        Scanner input = new Scanner (System.in);
+        double deposito = input.nextDouble();
+            saldo += deposito;
+            System.out.println("Seu saldo é :"+ saldo);
 
-    public void setNome_completo(String nome_completo) {
-        this.nome_completo = nome_completo;
     }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(Long saldo_inicial) {
-        this.saldo = saldo_inicial;
-    }
-    public void sacar(){}
-    public void deposito(){}   //PERGUNTAR O VALOR DE INPUT AQUI DENTRO DA CONTA
     public void investimento(){
         System.out.println("Digite o valor a investir: ");
         Scanner input = new Scanner (System.in);
@@ -44,7 +43,24 @@ public class Conta_Bancaria {
             System.out.println("Voce nao tem saldo pra investir");
         }
         }
-    public void dados_da_conta(){}
+
+    public void sacar_do_investimento(){
+        System.out.println("Digite o valor a retirar do investimento: ");
+        Scanner input = new Scanner (System.in);
+        double valorDoSaque = input.nextDouble();
+        if(valorDoSaque <= saldo_investido){
+            saldo_investido -= valorDoSaque;
+            saldo += valorDoSaque;
+            System.out.println("Seu saldo é :"+ saldo);
+            System.out.println("Seu saldo investido é :"+ saldo_investido);
+        }else{
+            System.out.println("Voce nao tem saldo pra sacar do investimento");
+        }
+    }
+    public void dados_da_conta(){
+        System.out.println("Seja bem vindo "+ nome_completo);
+        System.out.println("Seu saldo é :"+ saldo);
+    }
     public void bonus(){
         saldo_investido = saldo_investido * 1.05;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,22 +11,28 @@ public class Main {
             switch (escolha) {
                 case 1 -> contaBancaria1.deposito();
                 case 2 -> contaBancaria1.investimento();
-                case 3 -> contaBancaria1.exibir_saldos();
-                case 5 ->  System.out.println("Obrigado por usar o NuBank. Até a próxima!");
+                case 3 -> contaBancaria1.sacar();
+                case 4 -> contaBancaria1.sacar_do_investimento();
+                case 5 -> contaBancaria1.exibir_saldos();
+                case 6 -> contaBancaria1.dados_da_conta();
+                case 7 ->  System.out.println("Obrigado por usar o NuBank. Até a próxima!");
                 default -> System.out.println("Opção inválida. Escolha novamente.");
             }
             contaBancaria1.bonus();
-        }while(escolha < 5);
+        }while(escolha < 7);
 
     }
     public static int menuInicial(){
         Scanner input = new Scanner (System.in);
         System.out.println(" Bem vindo a NuBank");
-
+        // aparecer e apaga dps esse titulo
         System.out.println("1 Depositar");
         System.out.println("2 Investir ");
-        System.out.println("3 Exibir Saldos");
-        System.out.println("5 Sair");
+        System.out.println("3 Sacar");
+        System.out.println("4 Trasnferir do investimento para saldo");
+        System.out.println("5 Exibir Saldos");
+        System.out.println("6 Dados da Conta");
+        System.out.println("7 Sair");
         return input.nextInt();
     }
 
