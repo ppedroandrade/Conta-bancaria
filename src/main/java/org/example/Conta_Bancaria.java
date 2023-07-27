d3ad45a3f5f74c81ee0d2e824bf108a270400514
ppedroandrade <pedrosalvaro09@gmail.com>
2023-07-26 22:26:24 -0300

    Começando o menu e duas logicas

--- src/main/java/org/example/Conta_Bancaria.java
+++ src/main/java/org/example/Conta_Bancaria.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Conta_Bancaria {
     private String nome_completo;
-    private Long saldo_inicial;
+    private double saldo;
 
-    public Conta_Bancaria(String nome_completo, Long saldo_inicial) {
+    private double saldo_investido;
+
+    public Conta_Bancaria(String nome_completo, double saldo) {
         this.nome_completo = nome_completo;
-        this.saldo_inicial = saldo_inicial;
+        this.saldo = saldo;
     }
 
     public String getNome_completo() {
@@ -17,18 +21,37 @@ public class Conta_Bancaria {
         this.nome_completo = nome_completo;
     }
 
-    public Long getSaldo_inicial() {
-        return saldo_inicial;
+    public double getSaldo() {
+        return saldo;
     }
 
-    public void setSaldo_inicial(Long saldo_inicial) {
-        this.saldo_inicial = saldo_inicial;
+    public void setSaldo(Long saldo_inicial) {
+        this.saldo = saldo_inicial;
     }
     public String resgate(){return"Voce resgout ..."; }
-    public String deposito(){return"Voce resgout ..."; }
-    public String investimento(){return"Voce resgout ..."; }
+    public String deposito(){return"Voce resgout ...";}   //PERGUNTAR O VALOR DE INPUT AQUI DENTRO DA CONTA }
+    public void investimento(){
+        System.out.println("Digite o valor a investir: ");
+        Scanner input = new Scanner (System.in);
+        double valor = input.nextDouble();
+        if(valor <= saldo){
+            saldo -= valor;
+            saldo_investido += valor;
+            System.out.println("Seu saldo é :"+ saldo);
+            System.out.println("Seu saldo investido é :"+ saldo_investido);
+        }else{
+            System.out.println("Voce nao tem saldo pra investir");
+        }
+
+        }
     public String dados_da_conta(){return"Voce resgout ..."; }
-    public String bonus(){return"Voce resgout ..."; }
+    public void bonus(){
+        saldo_investido = saldo_investido * 1.05;
+    }
+    public void exibir_saldos(){
+        System.out.println("Seu saldo é :"+ saldo);
+        System.out.println("Seu saldo investido é :"+ saldo_investido);
+    }
 
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,31 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Conta_Bancaria contaBancaria1 = new Conta_Bancaria("Pedro Salvaro", 3L);
+        Conta_Bancaria contaBancaria1 = new Conta_Bancaria("Pedro Salvaro",100);
+        System.out.println(contaBancaria1.getNome_completo());
+        int escolha;
+        do{
+            escolha  = menuInicial();
 
+            switch (escolha) {
+                case 1 -> System.out.println(contaBancaria1.deposito());
+                case 2 -> contaBancaria1.investimento();
+                case 3 -> contaBancaria1.exibir_saldos();
+                default -> System.out.println("bbb");
+            }
+            contaBancaria1.bonus();
+        }while(escolha < 5);
+
+    }
+    public static int menuInicial(){
+        Scanner input = new Scanner (System.in);
+        System.out.println(" 1° Depositar");
+        System.out.println("2 Investir ");
+        System.out.println("3 Exibir Saldos");
+        System.out.println(" 5 Sair");
+        return input.nextInt();
     }
 }
